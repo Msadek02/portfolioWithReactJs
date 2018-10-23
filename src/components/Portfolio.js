d30480def10e4949a2a4e591e1c2434f689af8ea
Msadek02 <Thunder_axe_90@hotmail.com>
2018-10-23 14:05:48 -0400

    about and portfolio components

---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Portfolio = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Portfolio</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo obcaecati pariatur fugit qui minus itaque natus eos aspernatur. Quidem doloribus ut inventore facilis quos quas rem esse similique sed voluptas!</p>
+        </div>
+    )
+}
+
+export default Portfolio;

98991e5b7b053d639b6dbe82dcebdb30d90fed34
Msadek02 <Thunder_axe_90@hotmail.com>
2018-10-23 14:02:31 -0400

    homeComponent

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo obcaecati pariatur fugit qui minus itaque natus eos aspernatur. Quidem doloribus ut inventore facilis quos quas rem esse similique sed voluptas!</p>
+        </div>
+    )
+}
+
+export default Home;

71eeac5026da74258a671cfbf9f5057c366a69c1
Msadek02 <Thunder_axe_90@hotmail.com>
2018-10-26 16:27:11 -0400

    using axios and GET method to retrieve CNN news data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,50 @@
-import React from 'react'; 
-import ColorText from '../hoc/colorText';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo obcaecati pariatur fugit qui minus itaque natus eos aspernatur. Quidem doloribus ut inventore facilis quos quas rem esse similique sed voluptas!</p>
-        </div>
-    )
+
+class Home extends Component {
+    state = {
+        articles : []
+    }
+    componentDidMount(){
+        axios.get('https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=8b8ca926ec314a58b0c1cbb571d27fb1')
+            .then(res => {
+                this.setState({
+                    articles : res.data.articles
+                })
+                console.log(res);
+            })
+    }
+
+    render() {
+        const { articles } = this.state;
+        const articleList = articles.length ? (
+            articles.map(article => {
+                return(
+                    <div className="post card" key={article.source.id}>
+                        <div className="card-content">
+                            <span className="card-title">{article.title}</span>
+                            <p>{article.content}</p>
+                            <p>{article.description}</p>
+                            <a>{article.url}</a>
+                            <div className="container">
+                            <img src= {article.urlToImage} />
+                            </div>
+                            
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No Articles Yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {articleList}
+            </div>
+        )
+    }
 }
 
-export default ColorText(Home);
+export default Home;
